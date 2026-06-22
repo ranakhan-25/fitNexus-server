637c4b9e9c5240e80e2de6657c4bd11702a44b7c
Ranadev25 <ranakhandev2025@gmail.com>
2026-06-23 02:16:53 +0600

    modify ademin dashboard api

--- src/app.js
+++ src/app.js
@@ -29,7 +29,7 @@ async function run() {
     await client.connect();
 
     // await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    // console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
   }
 }
